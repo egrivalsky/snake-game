c6c967f0c1213564559ced49d669c5908a4838d7
unknown <erik@computer.com>
2021-05-27 22:34:12 -0400

    adds function for snake movement

--- script/index.js
+++ script/index.js
@@ -55,3 +55,12 @@ function moveOutcome() {
         moveSnake(squares);
     }
 }
+
+function moveSnake(squares) {
+    let tail = currentSnake.pop();
+    squares(tail).classList.remove("snake");
+    currentSnake.unshift(currentSnake[0] + direction);
+    //movement ends here
+    eatApple(squares, tail);
+    squares[currentSnake[0]].classList.add("snake")
+}
