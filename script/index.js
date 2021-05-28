62fad056f76072a549a8bb212ed5344d5aae216c
unknown <erik@computer.com>
2021-05-28 14:03:13 -0400

    adds random apple generator

--- script/index.js
+++ script/index.js
@@ -93,3 +93,9 @@ function eatApple(squares, tail) {
     }
 }
 
+function randomApple(squares) {
+    do {
+        appleIndex = Math.floor(Math.random() * squares.length);
+    } while (squares[appleIndex].classList.contains("snake"));
+    squares[appleIndex].classList.add("apple");
+}
